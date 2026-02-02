898e6a91614cddd598797e008c73dea2627c1576
temachy <temuchin26@gmail.com>
2026-02-02 11:19:31 +0200

    add More info button to Card component and rating wrapper

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,32 +1,29 @@
-import styles from './Card.module.scss'
-import { Rating } from '../Rating'
+import styles from "./Card.module.scss";
+import { Rating } from "../Rating";
 
 type CardProps = {
-    title: string
-    description: string
-    rating: number
-    image: string
-    onClick?: () => void
-}
+  title: string;
+  description: string;
+  rating: number;
+  image: string;
+  onClick?: () => void;
+};
 
-const Card = ({ title, description, rating, image, onClick }: CardProps) =>
-    <div
-        className={styles.card}
-        onClick={onClick}
-    >
+const Card = ({ title, description, rating, image, onClick }: CardProps) => (
+  <div className={styles.card}>
+    <img src={image} alt={title} className={styles.image} />
 
-        <img
-            src={image}
-            alt={title}
-            className={styles.image}
-        />
-
-        <div className={styles.content}>
-            <h2 className={styles.title}>{title}</h2>
-            <Rating rating={rating} />
-            <p className={styles.description}>{description}</p>
-        </div>
-
+    <div className={styles.content}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.rating_wrap}>
+        <Rating rating={rating} />
+        <button onClick={onClick} className={styles.button}>
+          More Info
+        </button>
+      </div>
+      <p className={styles.description}>{description}</p>
     </div>
+  </div>
+);
 
 export default Card;
