f58254152e996a227539874822894776e88071d5
temachy <temuchin26@gmail.com>
2026-02-02 11:17:26 +0200

    update Rating component styles and api

--- src/components/UI/Rating/Rating.tsx
+++ src/components/UI/Rating/Rating.tsx
@@ -1,26 +1,25 @@
-import styles from './Rating.module.scss'
+import styles from "./Rating.module.scss";
 
-const MAX_RATING = 5
+const MAX_RATING = 5;
 
 type RatingProps = {
-    rating: number
-}
+  rating?: number;
+};
 
-const Rating = ({ rating }: RatingProps) => (
-    <div className={styles.rating}>
-        {Array.from({ length: MAX_RATING }).map((_, index) => (
-            <span
-                key={index}
-                className={`${styles.star} ${index < rating ? styles.filled : styles.empty}`}
-            >
-                ★
-            </span>
-        ))}
-        <span className={styles.ratingText}>
-            ({rating}/{MAX_RATING})
-        </span>
-    </div>
-)
+const Rating = ({ rating = 0 }: RatingProps) => (
+  <div className={styles.rating}>
+    {Array.from({ length: MAX_RATING }).map((_, index) => (
+      <span
+        key={index}
+        className={`${styles.star} ${index < rating ? styles.filled : styles.empty}`}
+      >
+        ★
+      </span>
+    ))}
+    <span className={styles.ratingText}>
+      ({rating}/{MAX_RATING})
+    </span>
+  </div>
+);
 
-
-export default Rating
+export default Rating;
